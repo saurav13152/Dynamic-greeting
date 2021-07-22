85a2d316d8091996e5781e1692e93a8d1b4eb631
saurav13152 <chatterjee.saurav123@gmail.com>
2021-07-23 00:33:15 +0530

    Updated Time

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 let currdate = new Date();
-currdate = currdate.getHours();
-let greeting = "";
+var date = currdate.getHours();
+var currtime = currdate.toLocaleTimeString();
+let greeting = " ";
 //inline CSS
 const cSS = {};
 
-if (currdate >= 1 && currdate < 12) {
+if (date >= 1 && date < 12) {
   greeting = "Good Morning";
   cSS.color = "blue";
-} else if (currdate >= 12 && currdate < 20) {
+} else if (date >= 12 && date < 20) {
   greeting = "Good Afternoon";
   cSS.color = "orange";
 } else {
@@ -24,9 +25,10 @@ ReactDOM.render(
     <div>
       <h1>Welcome Saurav</h1>
     </div>
-    <h1>
+    <h2>
       <span style={cSS}>{greeting}</span>
-    </h1>
+    </h2>
+    <p> Current Time -{currtime}</p>
   </>,
   document.getElementById("root")
 );
